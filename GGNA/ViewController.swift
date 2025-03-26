dde91e1e7dd7f4b8e45cf3a466931b2c70f769ae
onesunny2 <ippne996@gmail.com>
2025-03-26 23:46:44 +0900

    first commit

---
+++ GGNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GGNA
+//
+//  Created by Lee Wonsun on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
