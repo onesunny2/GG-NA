d0e39b91a1d3fd08941f30079adb0b3f2d8e907d
onesunny2 <ippne996@gmail.com>
2025-03-29 17:41:07 +0900

    create ImageLiterals for sf symbol images

--- GGNA/Resource/Literals/ImageLiterals.swift
+++ GGNA/Resource/Literals/ImageLiterals.swift
@@ -10,17 +10,9 @@ import UIKit
 enum ImageLiterals {
     
     /// 앱 내 ViewController 화면에서 사용하는 Image Assets
-    static let plusApp: UIImage? = UIImage(systemName: "plus.app")
-    static let checkmarkSquare: UIImage? = UIImage(systemName: "checkmark.square.fill")
-    static let eye: UIImage? = UIImage(systemName: "eye.fill")
-    static let pencilCircle: UIImage? = UIImage(systemName: "pencil.circle")
-    static let starCircle: UIImage? = UIImage(systemName: "star.circle")
-    static let starFill: UIImage? = UIImage(systemName: "star.fill")
-    static let chevronRight: UIImage? = UIImage(systemName: "chevron.right")
     
     /// Navigation TabBar에서 사용하는 Image Assets
     static let home: UIImage? = UIImage(systemName: "house.fill")
-    static let search: UIImage? = UIImage(systemName: "magnifyingglass")
-    static let archive: UIImage? = UIImage(systemName: "archivebox")
-    static let rate: UIImage? = UIImage(systemName: "star.fill")
+    static let folder: UIImage? = UIImage(systemName: "folder.fill")
+    static let plus: UIImage? = UIImage(systemName: "plus")
 }
