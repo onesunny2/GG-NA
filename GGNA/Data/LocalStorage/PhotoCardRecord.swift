286667543df37b7fbc6cff690f231693c7b48ec5
onesunny2 <ippne996@gmail.com>
2025-03-30 19:10:21 +0900

    create DataModel For save at Realm

---
+++ GGNA/Data/LocalStorage/PhotoCardRecord.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCardRecord.swift
+//  GGNA
+//
+//  Created by Lee Wonsun on 3/30/25.
+//
+
+import Foundation
+import RealmSwift
+
+final class PhotoCardRecord: Object, Identifiable {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var imageData: Data?
+    @Persisted var videoData: Data?
+    @Persisted var filter: String
+    @Persisted var isSelectedMain: Bool
+    @Persisted var cardContent: CardContent?
+    
+    convenience init(
+        imageData: Data,
+        videoData: Data,
+        filter: String,
+        isSelectedMain: Bool,
+        cardContent: CardContent
+    ) {
+        self.init()
+        self.imageData = imageData
+        self.videoData = videoData
+        self.filter = filter
+        self.isSelectedMain = isSelectedMain
+        self.cardContent = cardContent
+    }
+}
+
---
+++ GGNA/Data/Repository/HomePhotoRepository.swift
@@ -0,0 +1,8 @@
+//
+//  HomePhotoRepository.swift
+//  GGNA
+//
+//  Created by Lee Wonsun on 3/30/25.
+//
+
+import Foundation
