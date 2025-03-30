def15cb807cb1770e3bfc90e92f67ee634324b59
onesunny2 <ippne996@gmail.com>
2025-03-30 19:13:16 +0900

    create Repository and DummyDate using it

--- GGNA/Data/Repository/HomePhotoRepository.swift
+++ GGNA/Data/Repository/HomePhotoRepository.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+import RealmSwift
+
+protocol HomePhotoRepository: AnyObject {
+    func getPhotosFromFolder() -> [PhotoCardRecord]
+}
+
+final class DefaultHomePhotoRepository {
+    
+}
+
+final class DummyHomePhotoRepository: HomePhotoRepository {
+    
+    func getPhotosFromFolder() -> [PhotoCardRecord] {
+        
+        return [
+            PhotoCardRecord(imageData: Data(), videoData: Data(), filter: "original", isSelectedMain: false, cardContent: CardContent()),
+            PhotoCardRecord(imageData: Data(), videoData: Data(), filter: "original", isSelectedMain: false, cardContent: CardContent()),
+            PhotoCardRecord(imageData: Data(), videoData: Data(), filter: "original", isSelectedMain: false, cardContent: CardContent()),
+            PhotoCardRecord(imageData: Data(), videoData: Data(), filter: "original", isSelectedMain: false, cardContent: CardContent()),
+            PhotoCardRecord(imageData: Data(), videoData: Data(), filter: "original", isSelectedMain: false, cardContent: CardContent())
+        ]
+    }
+}
