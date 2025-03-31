9a5f76954cf09f0ebb21ccd7ebd7ba6fbad35a1a
onesunny2 <ippne996@gmail.com>
2025-03-31 17:54:25 +0900

    change rootViewController at MainThread

--- GGNA/Resource/Extension/UIViewController+.swift
+++ GGNA/Resource/Extension/UIViewController+.swift
@@ -15,10 +15,15 @@ extension UIViewController {
         case .navigation:
             navigationController?.pushViewController(vc, animated: true)
         case .changeRootVC:
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first else { return }
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                  let window = windowScene.windows.first else { return }
             
-            window.rootViewController = vc
-            window.makeKeyAndVisible()
+            DispatchQueue.main.async {
+                UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: {
+                    window.rootViewController = vc
+                    window.makeKeyAndVisible()
+                }, completion: nil)
+            }
         case .fullScreen:
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: true)
