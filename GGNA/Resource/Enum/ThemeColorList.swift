93fe884a9c43fe2e26971840a9ef153c37e099c1
onesunny2 <ippne996@gmail.com>
2025-04-01 09:29:32 +0900

    create ThemeColorList using at ColorChangeModalSheet

--- GGNA/Resource/Enum/ThemeColorList.swift
+++ GGNA/Resource/Enum/ThemeColorList.swift
@@ -5,4 +5,23 @@
 //  Created by Lee Wonsun on 4/1/25.
 //
 
-import Foundation
+import UIKit
+
+enum ThemeColorList {
+    case darkPink
+    case lightPink
+    
+    var theme: String {
+        switch self {
+        case .darkPink: return "Dark"
+        case .lightPink: return "Light"
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .darkPink: return .ggDarkPink
+        case .lightPink: return .ggLightPink
+        }
+    }
+}
