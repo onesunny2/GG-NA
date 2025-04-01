08567b35755723c6ff0055ae9e2fa84891b93037
onesunny2 <ippne996@gmail.com>
2025-04-01 17:53:21 +0900

    create alert for addFolder at ArchiveVC

--- GGNA/Resource/Extension/UIViewController+.swift
+++ GGNA/Resource/Extension/UIViewController+.swift
@@ -28,4 +28,43 @@ extension UIViewController {
             present(vc, animated: true)
         }
     }
+    
+    func textFieldAlert() {
+        
+        let theme = CurrentTheme.currentTheme.theme
+        let color = CurrentTheme.currentTheme.color
+        let colors = color.setColor(for: theme)
+        
+        let alert = UIAlertController(
+            title: "새로운 폴더 이름",
+            message: nil,
+            preferredStyle: .alert
+        )
+        
+        // textfield 추가
+        alert.addTextField { textfield in
+            textfield.placeholder = "7자 이내로 적어주세요 :)"
+            textfield.autocapitalizationType = .none
+            textfield.autocorrectionType = .no
+            textfield.tintColor = colors.main
+        }
+        
+        let createAction = UIAlertAction(
+            title: "생성",
+            style: .default) { _ in
+                // 텍스트필드 입력된 값 가져오기
+                guard let text = alert.textFields?.first?.text else { return }
+                
+                print("입력한 텍스트: \(text)")
+            }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .default)
+        
+        alert.addAction(cancelAction)
+        alert.addAction(createAction)
+    
+        alert.view.tintColor = colors.main
+        
+        self.present(alert, animated: true)
+    }
 }
