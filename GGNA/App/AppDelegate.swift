2177e9937dd94c8f2a98bf2d81486b470b0827ca
onesunny2 <ippne996@gmail.com>
2025-04-12 01:13:28 +0900

    info P.list와 AppDelegate에서 카메라 실행 권환 확인 요청하도록 설정

--- GGNA/App/AppDelegate.swift
+++ GGNA/App/AppDelegate.swift
@@ -12,7 +12,9 @@ import RealmSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
+        
+        AppPermissionManager.requestCameraPermission()
+        
         migration()
         
         let realm = try! Realm()
