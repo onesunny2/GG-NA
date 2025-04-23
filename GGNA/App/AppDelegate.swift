2d101ba45168bfbc76b907f15e69126cbf680d19
onesunny2 <ippne996@gmail.com>
2025-04-23 14:43:57 +0900

    app push 장렬히 전사

--- GGNA/App/AppDelegate.swift
+++ GGNA/App/AppDelegate.swift
@@ -169,5 +169,16 @@ extension AppDelegate: MessagingDelegate {
             object: nil,
             userInfo: dataDict
         )
+        
+        // Topic 구독: "all_users" Topic에 등록
+        if let fcmToken = fcmToken {
+            Messaging.messaging().subscribe(toTopic: "all_users") { error in
+                if let error = error {
+                    print("Topic 구독 실패: \(error)")
+                } else {
+                    print("all_users Topic 구독 성공")
+                }
+            }
+        }
     }
 }
